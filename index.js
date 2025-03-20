17d067b228a7050c651a22077a2219862099d412
Derick Sheldrick <sheldrickderick9@gmail.com>
2025-03-20 13:38:42 +0300

    1st push assisgnment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+let array =[];
+function writeCards(array, message){
+    let messageArray = [];
+
+    for (let i = 0; i < array.length; i++) {
+        let  outputMessage =`Thank you, ${array[i]}, for the wonderful surprise gift!`
+    //    console.log (outputMessage);
+       messageArray.push(outputMessage);  
+         
+    }
+    return messageArray;
+}
+console.log(writeCards(['Guadalupe','Ollie','Aki']));
+
+function countDown(number){
+    let i= 0;
+    while (i<=10) {
+       console.log(10-i) 
+       i++;
+    }
+    return number;
+}
+countDown(10);
